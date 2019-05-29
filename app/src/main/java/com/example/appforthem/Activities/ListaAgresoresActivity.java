2592204d91621cdda989d0545c8c9faacca5a4c8
Jonathan <nathan20092411@gmail.com>
2019-05-28 23:03:06 -0500

    Se agrega la opción para agregar agresores

--- app/src/main/java/com/example/appforthem/Activities/ListaAgresoresActivity.java
+++ app/src/main/java/com/example/appforthem/Activities/ListaAgresoresActivity.java
@@ -4,21 +4,54 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
 
+import com.backendless.Backendless;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
 import com.example.appforthem.Clases.AdapterForAgresores;
+import com.example.appforthem.Clases.Agresor;
+import com.example.appforthem.Clases.BackendlessSettings;
 import com.example.appforthem.R;
+import com.github.ybq.android.spinkit.style.FadingCircle;
+
+import java.util.List;
 
 public class ListaAgresoresActivity extends AppCompatActivity {
     RecyclerView agresores;
     RecyclerView.LayoutManager layoutManager;
+    ProgressBar progressBar;
+    FadingCircle fadingCircle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_agresores);
-        agresores=findViewById(R.id.agresores);
-        layoutManager= new LinearLayoutManager(getApplicationContext());
-        agresores.setLayoutManager(layoutManager);
-        RecyclerView.Adapter adapter=new AdapterForAgresores(getApplicationContext());
-
+        progressBar = findViewById(R.id.pbAgresores);
+        fadingCircle = new FadingCircle();
+        progressBar.setProgressDrawable(fadingCircle);
+        agresores = findViewById(R.id.agresores);
+        layoutManager = new LinearLayoutManager(getApplicationContext());
+        progressBar.setVisibility(View.VISIBLE);
+        Backendless.Data.of(Agresor.class).find(new AsyncCallback<List<Agresor>>() {
+            @Override
+            public void handleResponse(List<Agresor> response) {
+                int size = response.size();
+                if (size > 0) {
+                    RecyclerView.Adapter adapter = new AdapterForAgresores(getApplicationContext(), response);
+                    agresores.setLayoutManager(layoutManager);
+                    agresores.setAdapter(adapter);
+                    progressBar.setVisibility(View.GONE);
+                } else {
+                    BackendlessSettings.showToast(getApplicationContext(),"No se han registrado agresores aún ");
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+            @Override
+            public void handleFault(BackendlessFault fault) {
+                BackendlessSettings.showToast(getApplicationContext(),"Erros mostrando agresores - "+fault.getMessage());
+            }
+        });
     }
 }
--- app/src/main/java/com/example/appforthem/Clases/AdapterForAgresores.java
+++ app/src/main/java/com/example/appforthem/Clases/AdapterForAgresores.java
@@ -3,41 +3,57 @@ package com.example.appforthem.Clases;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.backendless.Backendless;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
+import com.example.appforthem.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 public class AdapterForAgresores extends RecyclerView.Adapter<AdapterForAgresores.ViewHolder> {
 
-    BackendlessGeoUtils backendlessSettings = new BackendlessGeoUtils();
     List<Agresor> agresorList;
     Context context;
 
-    public AdapterForAgresores(Context context) {
-        this.context=context;
-        agresorList = backendlessSettings.getAgresores();
+    public AdapterForAgresores(Context context,List<Agresor> agresorList) {
+        this.context = context;
+        this.agresorList=agresorList;
+
     }
 
     @NonNull
     @Override
     public AdapterForAgresores.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_agresores, viewGroup, false);
+        return new AdapterForAgresores.ViewHolder(v);
+    }
+
+    public int getItemCount() {
+        return agresorList.size();
     }
 
     @Override
     public void onBindViewHolder(@NonNull AdapterForAgresores.ViewHolder viewHolder, int i) {
-
+        Picasso.get().load(agresorList.get(i).getPhoto_agresor()).transform(new CircleTransform()).into(viewHolder.imageView);
+        viewHolder.textView.setText(agresorList.get(i).getName_agresor());
     }
 
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView;
+        public TextView textView;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            imageView = itemView.findViewById(R.id.photo_agresor);
+            textView = itemView.findViewById(R.id.nombre_agresor);
         }
     }
 }
